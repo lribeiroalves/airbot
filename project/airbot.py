b010ddbda3d8bf6c2c71ff121c46439777b74f83
lribeiroalves <lribeiroalves@users.noreply.github.com>
2025-06-10 18:13:16 -0300

    novas atualizações

--- project/airbot.py
+++ project/airbot.py
@@ -1,24 +1,57 @@
 from playwright.sync_api import sync_playwright
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
+import re
 
 def main():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context(
+            locale='pt-BR',
+            user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            viewport={'width': 1920, 'height': 1080}
+        )
+        page = context.new_page()
 
-        page.goto('https://www.decolar.com/shop/flights/results/roundtrip/GRU/CDG/2025-07-03/2025-07-11/1/0/0?from=SB&di=1', timeout=60000)
-        page.wait_for_timeout(15000)
-        page.goto('https://www.decolar.com/shop/flights/results/roundtrip/GRU/AMS/2025-07-03/2025-07-11/1/0/0?from=SB&di=1', timeout=60000)
-        page.wait_for_timeout(5000)
+        page.goto('https://www.google.com/travel/flights', timeout=5000)
+        page.wait_for_timeout(300)
+
+        page.fill('input[aria-label="De onde?"]', 'GRU')
+        page.wait_for_timeout(300)
+        page.keyboard.press('Enter')
+        page.fill('input[aria-label="Para onde? "]', 'CDG')
+        page.wait_for_timeout(300)
+        page.keyboard.press('Enter')
+        page.wait_for_timeout(300)
+        page.fill('input[placeholder="Partida"]', '28/11/2025')
+        page.wait_for_timeout(300)
+        page.keyboard.press('Tab')
+        page.fill('input[placeholder="Volta"]', '12/12/2025')
+        page.wait_for_timeout(300)
+        page.keyboard.press('Tab')
+        page.wait_for_timeout(500)
+        page.keyboard.press('Enter')
+        page.wait_for_timeout(1000)
+        page.screenshot(path='s.png')
+        span = page.locator('span', has_text='Mostrar mais voos').nth(0)
+        span.click()
+        page.wait_for_timeout(300)
 
         html = page.content()
         soup = BeautifulSoup(html, 'html.parser')
-        preco = soup.select_one('span.price > p')
-        
-        print(preco)
-
         browser.close()
+
+        precos_str = soup.select('ul > li > div > div > div > div > div > div > div > div > span')
+        precos_int = [int(re.sub(r'\D', '', preco.get_text())) for preco in precos_str[:len(precos_str)//2]]
+        precos_int.sort()
+
+        for preco in precos_int[:5]:
+            print(preco)
+
 
 
 if __name__ == '__main__':
+    from datetime import datetime
+
+    tempo = datetime.now()
     main()
+    print(datetime.now() - tempo)
